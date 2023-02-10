bfd1e9f8cdc3fae8f9818de08e3cd75b840bc566
aelmahi <elmahi.aymane@gmail.com>
2023-02-10 11:05:25 +0100

    need to modify values

---
+++ build/CMakeFiles/findfunc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/findfunc.dir/findfunc.cpp.o"
+  "CMakeFiles/findfunc.dir/findfuncIndividual.cpp.o"
+  "findfunc"
+  "findfunc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/findfunc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- compile.sh
+++ compile.sh
@@ -1,4 +1,16 @@
-easena gravitationuniv.ez
-make 
-./gravitationuniv
-make easeaclean
+#!/bin/bash
+
+# if [ "$1" == "rm" ]; then
+#   rm *.log
+#   rm CMakeLists.txt
+# else 
+#   easena $1.ez
+#   make 
+#   ./$1 --nbCPUThreads 20 --seed 2
+#   make easeaclean
+# fi
+
+easena -gp findfunc.ez
+make
+./findfunc --nbCPUThreads 10 --seed 2
+
